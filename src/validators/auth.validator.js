a1821583a8cea859ca6dbf343c08757e6d79239d
avk-io <kashyapaditya29328@gmail.com>
2026-07-02 13:41:45 +0530

    add role to signup schema

--- src/validators/auth.validator.js
+++ src/validators/auth.validator.js
@@ -2,7 +2,8 @@ const { z } = require("zod")
 
 const signupSchema = z.object({
     email:z.string().email(),
-    password:z.string().min(6)
+    password:z.string().min(6),
+    role: z.enum(["buyer","seller"]).default("buyer")
 });
 
 const loginSchema = z.object({
